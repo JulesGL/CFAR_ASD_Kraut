e3d4cc55068ceaf5dded88841328e6e9eef201b2
JulesGL <jules.gomel@outlook.fr>
2023-11-28 11:48:47 +0100

    Added performance assessment of the detector

--- CFAR_ASD_GLRT.m
+++ CFAR_ASD_GLRT.m
@@ -72,16 +72,8 @@ invS = inv(S);
 % Real because sometimes complex with null imag part 
 cos2_hat = real(1 / (psi' * invS * psi) * (abs(psi' * invS * y)).^2 ./ (diag(y' * invS * y)'));
 
-%% 3)a) Threshold computation
-
-% Sort the test statistic 
-cos2_hat=sort(cos2_hat);
-
-% Find exp eta so that Nbr_fa=MC*Pfa
-eta_exp=cos2_hat(floor(K*(1-Pfa)));
-
-%% 3)b) Eta as a function of Pfa
-Pfa_vec=1E-2:1E-2:0.999;
+%% 3) Eta as a function of Pfa
+Pfa_vec=1E-4:1E-4:5E-1;
 eta_exp_array=zeros(1,length(Pfa_vec));
 
 for p=1:length(Pfa_vec)    
@@ -91,12 +83,12 @@ for p=1:length(Pfa_vec)
     % Number of measurements K : Number of Monte-Carlo samples
     K = floor(200 / Pfa);
 
-    % Generate synthetic measurements!
+    % Generate synthetic measurements under H0!
     W = sqrt(1/2) * (randn(N, K, 1) + 1i * randn(N, K, 1));
-    y = psi + sqrtm(sigma2 * R) * W;
+    y = sqrtm(sigma2 * R) * W;
     
     % Real because sometimes complex with null imag part 
-    cos2_hat = real(1 / (psi' * invS * psi) * (abs(psi' * invS * y)).^2 ./ (diag(y' * invS * y)'));
+    cos2_hat = real(1 / (psi' * invS * psi) * (abs(psi' * invS * y)).^2 ./ denom_cos2(y,invS,K));
 
     % Sort the test statistic 
     cos2_hat=sort(cos2_hat);
@@ -105,10 +97,127 @@ for p=1:length(Pfa_vec)
     eta_exp_array(p)=cos2_hat(floor(K*(1-Pfa)));
 end
 
+%% Display eta=f(Pfa)
+
 figure
 hold on
 xlabel('P_{fa}')
 ylabel('\eta')
 title('Threshold as a function of Probability of False alarm')
-plot(Pfa_vec,eta_exp_array,color='b',marker='pentagram')
+plot(Pfa_vec,eta_exp_array,color='b')
 hold off
+
+%% 3)b) Eta as a function of Pfa
+Pfa_vec=[1E-1,5E-2,1E-2,1E-3];
+
+sigma2_vec=1E-1:1E0:1E2;
+Pd_exp_vec=zeros(length(Pfa_vec),length(sigma2_vec));
+
+for p=1:length(Pfa_vec)
+    for i=1:length(sigma2_vec)
+        % Extract Pfa from vector
+        Pfa=Pfa_vec(p);
+        sigma2=sigma2_vec(i);
+        
+        % Number of measurements K : Number of Monte-Carlo samples
+        K = floor(200 / Pfa);
+
+        % Generate synthetic measurements under H0
+        W = sqrt(1/2) * (randn(N, K, 1) + 1i * randn(N, K, 1));
+        y_H0 = sqrtm(sigma2 * R) * W;
+
+        % Real because sometimes complex with null imag part 
+        cos2_hat_H0 = real(1 / (psi' * invS * psi) * (abs(psi' * invS * y_H0)).^2 ./ denom_cos2(y_H0,invS,K));
+
+        % Sort the test statistic 
+        cos2_hat_H0=sort(cos2_hat_H0);
+
+        % Find exp eta so that Nbr_fa=MC*Pfa
+        eta_exp=cos2_hat_H0(floor(K*(1-Pfa)));
+        
+        % Generate synthetic measurements under H1
+        W = sqrt(1/2) * (randn(N, K, 1) + 1i * randn(N, K, 1));
+        y_H1 = psi + sqrtm(sigma2 * R) * W;
+        
+        % Real because sometimes complex with null imag part 
+        cos2_hat_H1 = real(1 / (psi' * invS * psi) * (abs(psi' * invS * y_H1)).^2 ./ denom_cos2(y_H1,invS,K));
+
+        
+        Pd_exp_vec(p,i)=length(find(cos2_hat_H1>eta_exp))/K;
+    end
+end
+%%  Display
+figure
+hold on
+xlabel('\sigma^2')
+ylabel('P_d')
+title('P_d as function of \sigma^2, for a given P_{fa}')
+plot(sigma2_vec,Pd_exp_vec(1,:),color='r')
+plot(sigma2_vec,Pd_exp_vec(2,:),color='g')
+plot(sigma2_vec,Pd_exp_vec(3,:),color='b')
+plot(sigma2_vec,Pd_exp_vec(4,:),color='k')
+legend('P_{fa}=1E-1','P_{fa}=5E-2','P_{fa}=1E-2','P_{fa}=1E-3')
+hold off
+
+%% ROC - Pd = f(Pfa)
+
+Pfa_vec=1E-3:1E-3:1E-1;
+sigma2=20;
+Pd_ROC_vec=zeros(1,length(Pfa_vec));
+
+for p=1:length(Pfa_vec)
+        % Extract Pfa from vector
+        Pfa=Pfa_vec(p);
+        % Number of measurements K : Number of Monte-Carlo samples
+        K = floor(200 / Pfa);
+
+        % Generate synthetic measurements under H0
+        W = sqrt(1/2) * (randn(N, K, 1) + 1i * randn(N, K, 1));
+        y_H0 = sqrtm(sigma2 * R) * W;
+
+        % Real because sometimes complex with null imag part 
+        cos2_hat_H0 = real(1 / (psi' * invS * psi) * (abs(psi' * invS * y_H0)).^2 ./ denom_cos2(y_H0,invS,K));
+
+        % Sort the test statistic 
+        cos2_hat_H0=sort(cos2_hat_H0);
+
+        % Find exp eta so that Nbr_fa=MC*Pfa
+        eta_exp=cos2_hat_H0(floor(K*(1-Pfa)));
+        
+        % Generate synthetic measurements under H1
+        W = sqrt(1/2) * (randn(N, K, 1) + 1i * randn(N, K, 1));
+        y_H1 = psi + sqrtm(sigma2 * R) * W;
+        
+        % Real because sometimes complex with null imag part 
+        cos2_hat_H1 = real(1 / (psi' * invS * psi) * (abs(psi' * invS * y_H1)).^2 ./ denom_cos2(y_H1,invS,K));
+
+        
+        Pd_ROC_vec(p)=length(find(cos2_hat_H1>eta_exp))/K;
+
+end
+
+%% Display ROC
+
+figure
+hold on
+xlabel('P_{fa}')
+ylabel('P_d')
+title('P_d as function of P_{fa}, fixed SNR')
+plot(Pfa_vec,Pd_ROC_vec(:),color='b',marker='*')
+legend('P_d')
+hold off
+
+%% Functions
+
+function den=denom_cos2(y,invS,K)
+    den=zeros(1,K);
+    for k=1:K
+        %k-th column
+        y_k=y(:,k);
+        % Direct computation of the diag of y'S-1y to avoid huge arrays
+        den(k)=y_k'*invS*y_k;
+    end
+end
+
+
+
